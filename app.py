16392a202df6e661e169e746a95ac06bcc263247
DevMehta13 <139580977+DevMehta13@users.noreply.github.com>
2024-07-04 06:53:32 +0000

    form upadate

--- app.py
+++ app.py
@@ -43,9 +43,10 @@ def blind():
       gender = request.form.get("inlineRadioOptions")
       email = request.form.get("email")
       mobile_number = request.form.get("mobile_number")
-      adress = request.form.get("address")
+      address = request.form.get("address")
       occupation = request.form.get("occupation")
-      occupation_adress = request.form.get("occupation_adress")
+      occupation_address = request.form.get("occupation_address")
+      education = request.form.get("education")
       languages = request.form.getlist("languages")
 
       photo = request.files.get("photo")
@@ -75,12 +76,72 @@ def blind():
 
 @app.route("/book" , methods=["GET", "POST"])
 def book():
-   return render_template("book.html")
+
+   if request.method == "POST":
+      first_name = request.form.get("first_name")
+      last_name = request.form.get("last_name")
+      gender = request.form.get("inlineRadioOptions")
+      email = request.form.get("email")
+      mobile_number = request.form.get("mobile_number")
+      address = request.form.get("address")
+      occupation = request.form.get("occupation")
+      occupation_address = request.form.get("occupation_address")
+      education = request.form.get("education")
+      languages = request.form.getlist("languages")
+
+      audio = request.files.get("audio")
+
+      #TODO: Make the apology function and return the apology template
+      if not gender:
+         return "gender is required"
+      # else:
+      #    return f"{gender}"
+
+      if not languages:
+         return "Error: At least one language must be selected", 400
+      else:
+         return jsonify(languages)
+         # return f"Languages selected: {', '.join(languages)}"
+      # Example for how to add languages in languages column
+      # Insert the selected languages into the database
+      # cursor.execute("INSERT INTO user_languages (languages) VALUES (?)", (','.join(languages),))
+      
+
+   else:
+      return render_template("book.html")
 
 
 @app.route("/team" , methods=["GET", "POST"])
 def team():
-   return render_template("team.html")
+
+   if request.method == "POST":
+      first_name = request.form.get("first_name")
+      last_name = request.form.get("last_name")
+      gender = request.form.get("inlineRadioOptions")
+      email = request.form.get("email")
+      mobile_number = request.form.get("mobile_number")
+      address = request.form.get("address")
+      occupation = request.form.get("occupation")
+      occupation_address = request.form.get("occupation_address")
+      education = request.form.get("education")
+      about = request.form.get("about")
+      make_change = request.form.get("make_change")
+      aadhar_number = request.form.get("aadhar_number")
+      pan_number = request.form.get("pan_number")
+      aadhar_card = request.form.get("aadhar_card")
+      pan_card = request.form.get("pan_card")
+      photo = request.files.get("photo")
+
+      #TODO: Make the apology function and return the apology template
+      if not gender:
+         return "gender is required"
+      # else:
+      #    return f"{gender}"
+
+   else:
+      return render_template("team.html")
+
+   
 
 
 @app.route("/initiative")
